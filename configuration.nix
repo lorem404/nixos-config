d06c3801968732cdcbb0553f982ce8702c5f54fd
lorem <lorem8023@gmail.com>
2025-08-18 18:07:53 +0330

    moveing to home.nix
    move tmux and kitty configuration to home.nix file

--- configuration.nix
+++ configuration.nix
@@ -44,36 +44,6 @@
     shell ${pkgs.fish}/bin/fish
   '';
 
-  programs.tmux = {
-    enable = true;
-
-    # Basic settings
-    extraConfig = ''
-      # Terminal and mouse settings
-      set -g default-terminal "tmux-256color"
-      set -g mouse on
-      unbind C-b
-      set -g prefix C-s
-      bind s send-prefix
-
-      # Catppuccin plugin configuration
-      set -g @catppuccin_flavour "mocha"
-      set -g @catppuccin_window_status_style "rounded"
-      set -g @plugin 'tmux-plugins/tmux-cpu'
-
-
-      # Status bar configuration
-      set -g status-position top
-      set -g status-right-length 100
-      set -g status-left-length 100
-      set -g status-left ""
-      set -g status-right "#{E:@catppuccin_status_application}"
-      set -ag status-right "#{E:@catppuccin_status_session}"
-      set -ag status-right "#{E:@catppuccin_status_uptime}"
-      run-shell ${pkgs.tmuxPlugins.catppuccin.rtp}
-    '';
-  };
-
   programs.starship = {
     enable = true;
     # Optional: Custom settings (default: ~/.config/starship.toml)
--- home.nix
+++ home.nix
@@ -6,6 +6,64 @@
 
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
+  programs.tmux = {
+    enable = true;
+
+    # Basic settings
+    terminal = "tmux-256color";
+    mouse = true;
+
+    # Plugins
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+        '';
+      }
+      cpu
+      battery
+    ];
+
+    extraConfig = ''
+      # Status bar configuration
+      set -g status-position top
+      unbind C-b
+      set -g prefix C-s
+      bind s send-prefix
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
+      run-shell ${pkgs.tmuxPlugins.catppuccin.rtp}
+    '';
+  };
+
+  programs.kitty = {
+    enable = true;
+
+    font = {
+      name = "FiraCode Nerd Font";
+      size = 12.0;
+    };
+
+    settings = {
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
+      cursor_trail = 100;
+      hide_window_decorations = "yes";
+      background_opacity = 100;
+      background = "#1e1e2e";
+      foreground = "#cdd6f4";
+    };
+
+    shellIntegration.enableFishIntegration = true;
+  };
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [ ];
