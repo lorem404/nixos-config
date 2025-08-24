79725cecdb7dd2057c9e492ad1d7560912af66de
lorem <lorem8023@gmail.com>
2025-08-24 12:50:03 +0330

    installing gh

--- configuration.nix
+++ configuration.nix
@@ -165,6 +165,7 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     glib
+    gh
     pkg-config
     libseccomp
     gnumake
