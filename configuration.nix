7f610f2d561362fdfde521520bcad68f443cd358
lorem <lorem8023@gmail.com>
2025-08-24 12:43:02 +0330

    fix bug
    changing gvlib to glib

--- configuration.nix
+++ configuration.nix
@@ -164,7 +164,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    gvlib
+    glib
     pkg-config
     libseccomp
     gnumake
