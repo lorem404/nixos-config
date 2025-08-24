72b6b315d81cae8f316412b7b5e38a918024ba35
lorem <lorem8023@gmail.com>
2025-08-24 12:25:24 +0330

    installing gnumake

--- configuration.nix
+++ configuration.nix
@@ -164,6 +164,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    gnumake
     wget
     fish
     neovim
